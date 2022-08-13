4a3d41161b1dcebf0938dcff847b69bbe69d57b9
Ana <9025015386@gmail.ru>
2022-08-13 22:05:37 +0500

    ветка 1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+//Задание 1
+        int[] weight = new int[12];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
+        double[] weifhts = {1.57, 7.654, 9.986};
+        {
+            int[] box = new int[5];
+            box[0] = 22;
+            box[1] = 1;
+            box[2] = 25;
+            box[3] = 11;
+            box[4] = 21;
+            char[] sammerDays = {12, 22, 31};
+        }
+// Задача 2
+        for (int i = 0; i <= 3; i++) {
+            System.out.print(weight[i] + ", ");
+
+        }
+        }
+    }
